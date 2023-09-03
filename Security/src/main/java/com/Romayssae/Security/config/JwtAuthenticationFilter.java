22a3791cf32c86253b43fdebbc01013e0e6aef61
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-03 21:54:56 +0100

    Check the JWT token and extract the token from the authentication header

--- Security/src/main/java/com/Romayssae/Security/config/JwtAuthenticationFilter.java
+++ Security/src/main/java/com/Romayssae/Security/config/JwtAuthenticationFilter.java
@@ -20,6 +20,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @Nonnull HttpServletResponse response,
             @Nonnull FilterChain filterChain)
             throws ServletException, IOException {
+        //Check the validity of JWT token
+        final String authHeader = request.getHeader("Authorization"); //Header that contains the JWT token
+        final String jwt;
+        if (authHeader==null || !authHeader.startsWith("Bearer")){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //Extract the token from the authentication header
+        jwt= authHeader.substring(7); //Bearer already occupied 6
 
     }
+
 }
