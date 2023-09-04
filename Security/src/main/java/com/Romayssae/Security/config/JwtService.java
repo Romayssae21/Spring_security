f7078356b6413e1b398980e8b278252a78242f0a
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-04 12:07:47 +0100

    Implement method to generate token

--- Security/src/main/java/com/Romayssae/Security/config/JwtService.java
+++ Security/src/main/java/com/Romayssae/Security/config/JwtService.java
@@ -2,12 +2,16 @@ package com.Romayssae.Security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.apache.el.lang.FunctionMapperImpl;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -20,6 +24,21 @@ public class JwtService {
     public <T> T extractedClaim(String token, Function<Claims,T> claimResolver){
         final Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
+    }
+
+    public String generateToken(
+            Map<String ,Object> extraClaims,
+            UserDetails userDetails
+    ){
+
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*24)) //Valid for 24h + 1000ms
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
     private Claims extractAllClaims(String token){
         return Jwts
