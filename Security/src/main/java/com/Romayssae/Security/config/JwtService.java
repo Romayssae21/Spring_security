ce7352cdd275172c68a150ec7f8d3a013114d845
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-04 08:12:24 +0100

    Implement the jjwt dependencies that we've added to our service class by Creating a method that allows us to extract all the claims. The created method "extractAllClaims" takes a JWT string (token), verifies its authenticity, and extracts the information (claims) contained within it

--- Security/src/main/java/com/Romayssae/Security/config/JwtService.java
+++ Security/src/main/java/com/Romayssae/Security/config/JwtService.java
@@ -1,10 +1,32 @@
 package com.Romayssae.Security.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY= "JOVqqJGrESUR/4UcePTpzjwaYde3qQNa38XChjTbFRe52dFAA8S6ahENFYUDr/aX";
     public String extractUserName(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                //set the signing key used to verify the signature of the JWT
+                .setSigningKey(getSigningKey())
+                .build() //parser instance ready for use
+                .parseClaimsJws(token) //verifies the "token" signature
+                //return a Claims object that contains the decoded JSON claims.
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
