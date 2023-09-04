55819c50366e33dbc1ea69fcaf88511dcb349e5d
Romayssae21 <romayssae.21.chan@gmail.com>
2023-09-04 08:21:33 +0100

    Create the "extractedClaim" method that allows us to extract one single claim and any claim

--- Security/src/main/java/com/Romayssae/Security/config/JwtService.java
+++ Security/src/main/java/com/Romayssae/Security/config/JwtService.java
@@ -4,15 +4,22 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.apache.el.lang.FunctionMapperImpl;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY= "JOVqqJGrESUR/4UcePTpzjwaYde3qQNa38XChjTbFRe52dFAA8S6ahENFYUDr/aX";
     public String extractUserName(String token) {
         return null;
+    }
+    //Generic method:
+    public <T> T extractedClaim(String token, Function<Claims,T> claimResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
     }
     private Claims extractAllClaims(String token){
         return Jwts
